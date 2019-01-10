4e48df07d7920a6b56cb2b1b3aec760909fd1a36
MattisBrizard <mattisbrizard@icloud.com>
2019-01-10 19:39:29 +0100

    added google signin for iOS

---
+++ lib/login.dart
@@ -0,0 +1,263 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'package:auto_size_text/auto_size_text.dart';
+import 'main.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CrowdEffect',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: 'Montserrat',
+      ),
+      home: LoginScreen(title: 'CrowdEffect'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  // SharedPreferences prefs;
+
+  bool isLoading = false;
+  bool isLoggedIn = false;
+  FirebaseUser currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    isSignedIn();
+  }
+
+  void isSignedIn() async {
+    this.setState(() {
+      isLoading = true;
+    });
+
+    // prefs = await SharedPreferences.getInstance();
+
+    isLoggedIn = await googleSignIn.isSignedIn();
+    if (isLoggedIn) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ChatRoomSelectionPage(currentUserId: currentUser.uid)
+          // builder: (context) => ChatRoomSelectionPage()
+          ),
+      );
+    }
+
+    this.setState(() {
+      isLoading = false;
+    });
+  }
+
+  Future<Null> handleSignIn() async {
+    // prefs = await SharedPreferences.getInstance();
+
+    this.setState(() {
+      isLoading = true;
+    });
+
+    try{
+      GoogleSignInAccount googleUser = await googleSignIn.signIn().catchError((onError){
+        print("Error $onError");
+        this.setState(() {
+          isLoading = false;
+        });
+        });
+      if (googleUser != null) {
+      GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      FirebaseUser firebaseUser = await firebaseAuth.signInWithGoogle(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      ).catchError((onError) {
+            print("error $onError");
+            this.setState(() {
+              isLoading = false;
+            });
+      });
+      if (firebaseUser != null) {
+        // Check is already sign up
+        final QuerySnapshot result =
+            await Firestore.instance.collection('users').where('id', isEqualTo: firebaseUser.uid).getDocuments();
+        final List<DocumentSnapshot> documents = result.documents;
+        if (documents.length == 0) {
+          // Update data to server if new user
+          Firestore.instance
+              .collection('users')
+              .document(firebaseUser.uid)
+              .setData({'nickname': firebaseUser.displayName, 'photoUrl': firebaseUser.photoUrl, 'id': firebaseUser.uid});
+
+          // Write data to local
+          currentUser = firebaseUser;
+          // await prefs.setString('id', currentUser.uid);
+          // await prefs.setString('nickname', currentUser.displayName);
+          // await prefs.setString('photoUrl', currentUser.photoUrl);
+        } else {
+          // Write data to local
+          // await prefs.setString('id', documents[0]['id']);
+          // await prefs.setString('nickname', documents[0]['nickname']);
+          // await prefs.setString('photoUrl', documents[0]['photoUrl']);
+        }
+        Fluttertoast.showToast(msg: "Sign in success");
+        this.setState(() {
+          isLoading = false;
+        });
+
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => ChatRoomSelectionPage(
+                    currentUserId: firebaseUser.uid,
+                  )
+              ),
+        );
+      } else {
+        Fluttertoast.showToast(msg: "Sign in fail");
+        this.setState(() {
+          isLoading = false;
+        });
+      }
+    }
+    else{
+      Fluttertoast.showToast(msg: "Sign in fail");
+      this.setState(() {
+        isLoading = false;
+      });
+    }
+    }
+    catch(e){
+      print("Error $e");
+      this.setState(() {
+          isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaHeight = MediaQuery.of(context).size.height;
+    return new Scaffold(
+        resizeToAvoidBottomPadding: false,
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              stops: [0.0, 1.0],
+              colors: [
+                Color.fromRGBO(65, 67, 69, 1.0),
+                Color.fromRGBO(35, 37, 38, 1.0)
+              ],
+            ),
+          ),
+          child: Stack(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: SafeArea(
+                child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    height: mediaHeight * 0.25,
+                    alignment: Alignment(-1.0,0.0),
+                      child:  AutoSizeText(
+                        'Crowd Effect',
+                        style: TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold),
+                        maxLines: 2,
+                      ),
+                    ),
+                  Expanded(
+                    child: Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                            Container(
+                              height: mediaHeight * 0.07,
+                              child: Material(
+                                borderRadius: BorderRadius.circular(20.0),
+                                shadowColor: Colors.black,
+                                color: Colors.grey,
+                                elevation: 7.0,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                          color: Colors.black,
+                                          style: BorderStyle.solid,
+                                          width: 1.0
+                                      ),
+                                      borderRadius: BorderRadius.circular(20.0)),
+                                  child: InkWell(
+                                    borderRadius: BorderRadius.circular(20.0),
+                                    onTap: () {
+                                      handleSignIn();
+                                    },
+                                    child: 
+                                        Center(
+                                          child: Text('Sign in with Google',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  )
+                                                ),
+                                        ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                        ],
+                      ),
+                    )
+                    ),
+                    Container(
+                      height: mediaHeight * 0.1,
+                      child: Center(
+                        child: Text(
+                          'App made by Emrin & Mattis',
+                        ),
+                      ),
+                    ),
+                ],
+            ),
+            ),
+              ),
+            // Loading
+            Positioned(
+              child: isLoading
+                  ? Container(
+                      child: Center(
+                        child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+                        ),
+                      ),
+                      color: Colors.white.withOpacity(0.8),
+                    )
+                  : Container(),
+            ),
+          ]
+          ),
+        )
+      );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,168 +1,202 @@
-import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 
-import 'signup.dart';
-import 'room_selection.dart';
-import 'room.dart';
-
+import 'login.dart';
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+bool _showInfo = false;
+
+class ChatRoomSelectionPage extends StatefulWidget {
+  final String currentUserId;
+
+  ChatRoomSelectionPage({Key key, @required this.currentUserId}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: <String, WidgetBuilder>{
-        '/signup': (BuildContext context) => new SignupPage(),
-        '/roomselection': (BuildContext context) => new ChatRoomSelectionPage(),
-        '/room': (BuildContext context) => new ChatRoomPage()
-      },
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Montserrat',
-      ),
-      home: new MyHomePage(),
-    );
-  }
+  ChatRoomSelectionPageState createState() => new ChatRoomSelectionPageState(currentUserId: currentUserId);
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
+class ChatRoomSelectionPageState extends State<ChatRoomSelectionPage>{
+
+  ChatRoomSelectionPageState({Key key, @required this.currentUserId});
+  final String currentUserId;
+
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  Future<Null> _handleSignOut() async {
+    // this.setState(() {
+    //   isLoading = true;
+    // });
+
+    await FirebaseAuth.instance.signOut();
+    await googleSignIn.disconnect();
+    await googleSignIn.signOut();
+
+    // this.setState(() {
+    //   isLoading = false;
+    // });
+
+    Navigator.of(context)
+        .pushAndRemoveUntil(MaterialPageRoute(builder: (context) => MyApp()), (Route<dynamic> route) => false);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
   @override
-  Widget build(BuildContext context) {
-    final mediaHeight = MediaQuery.of(context).size.height;
-    return new Scaffold(
-        resizeToAvoidBottomPadding: false,
+    Widget build(BuildContext context) {
+      final mediaSize = MediaQuery.of(context).size;
+      return Scaffold(
         body: Container(
+          // Add box decoration
           decoration: BoxDecoration(
+            // Box decoration takes a gradient
             gradient: LinearGradient(
+              // Where the linear gradient begins and ends
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
+              // Add one stop for each color. Stops should increase from 0 to 1
               stops: [0.0, 1.0],
               colors: [
+                // Colors are easy thanks to Flutter's Colors class.
                 Color.fromRGBO(65, 67, 69, 1.0),
                 Color.fromRGBO(35, 37, 38, 1.0)
               ],
             ),
           ),
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
           child: SafeArea(
             child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                height: mediaHeight * 0.25,
-                alignment: Alignment(-1.0,0.0),
-                  // child: Stack(
-                  //   children: <Widget>[
-                  //     Container(
-                  //       child: Text('Crowd',
-                  //           style: TextStyle(
-                  //               fontSize: 80.0, fontWeight: FontWeight.bold
-                  //             )
-                  //         ),
-                  //     ),
-                  //     Container(
-                  //       padding: EdgeInsets.only(top: 70.0),
-                  //       child: Text('Effect',
-                  //           style: TextStyle(
-                  //               fontSize: 80.0, fontWeight: FontWeight.bold)),
-                  //     ),
-                  //   ],
-                  // ),
-                  child:  AutoSizeText(
-                    'Crowd Effect',
-                    style: TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold),
-                    maxLines: 2,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  child : Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                        height: mediaSize.height * 0.25,
+                        alignment: Alignment(-1.0,0.0),
+                          child:  AutoSizeText(
+                            'Crowd Effect',
+                            style: TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold),
+                            maxLines: 2,
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.settings,
+                          size: 35.0,
+                        ),
+                        onPressed: (){
+                          _handleSignOut();
+                        },
+                      )
+                    ],
                   ),
                 ),
-              Expanded(
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                        Container(
-                          height: mediaHeight * 0.07,
-                          child: Material(
-                            borderRadius: BorderRadius.circular(20.0),
-                            shadowColor: Colors.black,
-                            color: Colors.grey,
-                            elevation: 7.0,
+                Expanded(
+                  child: CarouselSlider(
+                    items: [1,2,3,4,5].map((i) {
+                      return new Builder(
+                        builder: (BuildContext context) {
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).pushNamed('/room');
+                            },
                             child: Container(
-                              decoration: BoxDecoration(
-                                  border: Border.all(
-                                      color: Colors.black,
-                                      style: BorderStyle.solid,
-                                      width: 1.0
+                              width: mediaSize.width * 0.7,
+                              // margin: new EdgeInsets.symmetric(horizontal: 10.0),
+                              decoration: new BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                              ),
+                              child: Column(
+                                children: <Widget>[
+                                  Container(
+                                    height: mediaSize.height * 0.25,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.vertical(top: Radius.circular(16.0)),
+                                      image: DecorationImage(
+                                        // image: NetworkImage('https://source.unsplash.com/collection/$i'),
+                                        image: AssetImage('assets/img.jpg'),
+                                        fit: BoxFit.cover
+                                      )
+                                    ),
                                   ),
-                                  borderRadius: BorderRadius.circular(20.0)),
-                              child: InkWell(
-                                borderRadius: BorderRadius.circular(20.0),
-                                onTap: () {
-                                  Navigator.pushReplacementNamed(context, "/roomselection");
-                                },
-                                child: 
-                                    Center(
-                                      child: Text('Sign in with Google',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              )
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 18.0, right: 18.0, top: 24.0),
+                                    child: Row(
+                                      children: <Widget>[
+                                        Expanded(
+                                          child: Text('Title $i',
+                                            style: TextStyle(
+                                              fontSize: 25.0,
+                                              color: Colors.black
                                             ),
-                                    ),
-                              ),
-                            ),
-                          ),
-                        ),
-                        SizedBox(height: mediaHeight * 0.05),
-                        Container(
-                          height: mediaHeight * 0.07,
-                          color: Colors.transparent,
-                          child: Material(
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  border: Border.all(
-                                      color: Colors.black,
-                                      style: BorderStyle.solid,
-                                      width: 1.0),
-                                  color: Colors.transparent,
-                                  borderRadius: BorderRadius.circular(20.0)),
-                              child: InkWell(
-                                borderRadius: BorderRadius.circular(20.0),
-                                onTap: () {
-                                },
-                                child: 
-                                    Center(
-                                      child: Text('Sign up with Google',
+                                          )
+                                        ),
+                                        Text('$i/10',
                                           style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontFamily: 'Montserrat')),
+                                            fontSize: 12.0,
+                                            color: Colors.black
+                                          ),
+                                        ),
+                                      ],
                                     ),
-                              ),
+                                  ),
+                                  Expanded(
+                                      child: Padding(
+                                        padding: EdgeInsets.symmetric(vertical: 18.0, horizontal: 20.0),
+                                         child: SingleChildScrollView(
+                                           child: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac ante in mi pellentesque scelerisque.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac ante in mi pellentesque scelerisque.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac ante in mi pellentesque scelerisque.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac ante in mi pellentesque scelerisque.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac ante in mi pellentesque scelerisque.',
+                                                style: TextStyle(
+                                                  fontSize: 12.0,
+                                                  color: Colors.grey
+                                                ),
+                                        ),
+                                    ),
+                                      ),
+                                  )
+                                ],
+                              )
                             ),
-                          ),
+                          );
+                        },
+                      );
+                    }).toList(),
+                    height: mediaSize.height * 0.5,
+                    distortion: true,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  alignment: Alignment(0, 0),
+                  child: Column(
+                    children: <Widget>[
+                      AnimatedOpacity(
+                        opacity: _showInfo ? 1.0 : 0.0,
+                        duration: Duration(milliseconds: 500),
+                        child: Text('Click on a card to enter the room')
+                      ),
+                      IconButton(
+                        onPressed: (){
+                          setState(() {
+                             _showInfo = !_showInfo;
+                          });
+                        },
+                        icon: Icon(
+                          Icons.info_outline,
+                          size: 35.0
                         ),
+                      ),
                     ],
                   ),
                 )
-                ),
-                Container(
-                  height: mediaHeight * 0.05,
-                  child: Center(
-                    child: Text(
-                      'App made by Emrin & Mattis',
-                    ),
-                  ),
-                ),
-            ],
-          ),
-          ),
-        )
+              ],
+            ),
+          )
+        ),
       );
-  }
+    }
 }
